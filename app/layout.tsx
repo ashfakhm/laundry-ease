18f55bc1e10abbf60b465633e592d6294ebcb2d6
ASHFAKH M <editorashfakh@gmail.com>
2025-12-14 11:31:13 +0530

    added theme property but now users preference on system is worked i didnt added new button or drop down to change themes if browsers setup is dark dark is applied whole if not white theme is applied everywhere

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
+import { ThemeProvider } from "./components/ui/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,8 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className="dark">
-        <body>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+              {children}
+            </main>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
