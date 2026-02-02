d8facda5b765c9d31859014e3c5ab0ff9ed92142
ASHFAKH M <editorashfakh@gmail.com>
2026-02-02 14:44:30 +0530

    fixed robots function to use an array for rules and include additional disallowed paths

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,14 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://laundryease.in";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin/", "/api/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/"],
+        disallow: ["/admin/", "/api/", "/complete-signup/", "/choose-role/"],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
